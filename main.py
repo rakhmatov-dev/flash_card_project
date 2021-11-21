d111aebcb9a56e6ef822c55f7b23cec9841fe5f4
rakhmatov93khondamir <khondamir.rakhmatov@gmail.com>
2021-11-22 01:08:27 +0500

    First buttons added

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+import tkinter as tk
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+window = tk.Tk()
+window.title("Flash Card Project")
+window.config(padx=100, pady=50, bg=BACKGROUND_COLOR)
+
+right_image = tk.PhotoImage(file="/Volumes/Extreme SSD/Python Bootcamp/Day 31/Mac/flash_card_project/images/right.png")
+correct_button = tk.Button(image=right_image, highlightthickness=0)
+correct_button.pack()
+
+wrong_image = tk.PhotoImage(file="/Volumes/Extreme SSD/Python Bootcamp/Day 31/Mac/flash_card_project/images/wrong.png")
+wrong_button = tk.Button(image=wrong_image, highlightthickness=0)
+wrong_button.pack()
+
+
+window.mainloop()
+
+
+
 
